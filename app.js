cbbbaf8867e041bf9c92cb9d6df6861f0e8ca850
Adel Jojo <bigfanjs@gmail.com>
2017-10-19 16:13:55 +0100

    feat: disables the view engine

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ mongoose.connect(config.get("database.uri"));
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
@@ -34,22 +30,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.post("/signup", validateSchema("user"), users.signup);
 app.post("/signin", validateSchema("user"), auth.authenticate(), users.signin);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(404).json({error: new Error("Not Found")});
 });
 
 module.exports = app;
